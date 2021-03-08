f616e753f078b8edd92c51f3d3fcf0eb1be15500
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-08 15:41:56 +0300

    Remove bootstrap import in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import App from './App';
