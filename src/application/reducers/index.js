9ae68b15f1ee6923439f1c8c60c1b13c70b6093c
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-17 21:45:51 +0300

    Add search reducer to root reducer

--- src/application/reducers/index.js
+++ src/application/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import trendingReducer from './trending'
 import popularReducer from './popular';
+import searchReducer from './search'
 
 const rootReducer= combineReducers({
   trendings:trendingReducer,
-  populars:popularReducer
+  populars:popularReducer,
+  search:searchReducer
 });
 
 export default rootReducer;
