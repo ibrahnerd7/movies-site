9f4f1d23c641f8bb8a32ffbf2401a4fcbf6b2d06
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-17 21:44:24 +0300

    Create search reducer

---
+++ src/application/reducers/searchReducer.js
@@ -0,0 +1,34 @@
+import { SEARCH } from "../constants";
+
+const initialState = {
+    results: [],
+    loading: false,
+    error: false
+}
+
+const searchReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SEARCH.LOAD:
+            return {
+                results: [],
+                loading: true,
+                error: false,
+            };
+        case SEARCH.LOAD_SUCCESS:
+            return {
+                trendings: action.trendings,
+                loading: false,
+                error: false
+            };
+        case SEARCH.LOAD_FAIL:
+            return {
+                results: [],
+                loading: false,
+                error: true
+            };
+        default:
+            return state;
+    }
+};
+
+export default searchReducer;
