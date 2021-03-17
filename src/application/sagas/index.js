f1a54cb4f0e771f7f8966da68dade826157ee66c
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-17 22:55:18 +0300

    Include search saga in rootSaga

--- src/application/sagas/index.js
+++ src/application/sagas/index.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 import trendingSaga from './trendingSaga';
 import popularSaga from './popularSaga';
+import searchSaga from './searchSaga';
 
 export default function* rootSaga(){
-    yield all([trendingSaga(),popularSaga()]);
+    yield all([trendingSaga(),popularSaga(),searchSaga()]);
 }
--- src/views/search/Search.js
+++ src/views/search/Search.js
@@ -1,29 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation } from 'react-router';
 import {
     Container, CardText, Card,
-   CardBody
+   CardBody,Row
 } from 'reactstrap';
+import { requestSearch } from '../../application/actions-creators/search';
 
 const useQuery=()=>{
     return new URLSearchParams(useLocation().search)
 }
 
+const SearchResultsCard=()=>{
+    return ( <Card className={"flex-1 flex-row"}>
+    <img width="9%" height="116" src="https://www.themoviedb.org/t/p/w94_and_h141_bestv2/qAe4MS10QyDRYpUjVr5KxQtIq7V.jpg" alt="Card image cap" />
+    <CardBody>
+        <h6>Mysterious Doctor Satan, The</h6>
+        <CardText>CRAZED WITH THE LUST TO KILL! A power-mad maniac unleashes a lightning attack on an unsuspecting nation...And finds the Copperhead thirsting for revenge! A mad scientist named Dr. Satan plots to steal key pieces of technology to enable him to build an</CardText>
+    </CardBody>
+</Card>)
+}
+
 const Search = ({ }) => {
     let query=useQuery();
-    const searchTerm=query.get("query");
+    const [searchTerm,setSearchTerm]=useState(query.get("query"));
+    const {results,loading}=useSelector(state=>state.search);
+    const dispatch=useDispatch()
+  
+    useEffect(()=>{
+    dispatch(requestSearch(searchTerm));
+    },[searchTerm])
+
 
     return (
         <Container >
-            <main className="mt-4">
-                <Card className={"flex-1 flex-row"}>
-                    <img width="9%" height="116" src="https://www.themoviedb.org/t/p/w94_and_h141_bestv2/qAe4MS10QyDRYpUjVr5KxQtIq7V.jpg" alt="Card image cap" />
-                    <CardBody>
-                        <h6>Mysterious Doctor Satan, The</h6>
-                        <CardText>CRAZED WITH THE LUST TO KILL! A power-mad maniac unleashes a lightning attack on an unsuspecting nation...And finds the Copperhead thirsting for revenge! A mad scientist named Dr. Satan plots to steal key pieces of technology to enable him to build an</CardText>
-                    </CardBody>
-                </Card>
-            </main>
+           <div>
+           <h3>Results</h3>
+            <Row className="flex-nowrap flex-row trending flex-1" >
+                    {
+                    results.map((searchResult) => <SearchResultsCard item={searchResult} key={searchResult.id} />)
+                }
+                </Row>
+        </div>
+               
+            
         </Container>
     )
 }
