d32473826d4970483bb35d0c6a366418b1cb9f3a
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-03-18 20:53:05 +0300

    Include shows reducer in root reducer

--- src/application/reducers/index.js
+++ src/application/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import trendingReducer from './trending'
 import popularReducer from './popular';
 import searchReducer from './search'
+import showsReducer from './shows'
 
 const rootReducer= combineReducers({
   trendings:trendingReducer,
   populars:popularReducer,
-  search:searchReducer
+  search:searchReducer,
+  shows:showsReducer
 });
 
 export default rootReducer;
