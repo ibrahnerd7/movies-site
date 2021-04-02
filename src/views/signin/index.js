7efcdb17a9528804b166db73e38a86a2591554c2
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-04-02 10:15:34 +0300

    Login users successfully

--- src/views/signin/index.js
+++ src/views/signin/index.js
@@ -1,10 +1,25 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 import { Col, Container, Input, Row, Button } from 'reactstrap';
-
+import { loginUser } from '../../infrastructure/services/firebase/auth'
 const SignIn = () => {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState();
 
+    const history = useHistory()
 
+    const signInUser = async (e) => {
+        e.preventDefault()
+        let user = await loginUser(email, password);
+        console.log(user)
+        if (!user.uid) {
+            setError(user.message)
+        } else {
+            history.push("/")
+        }
+
+    }
 
     return (
         <Container >
@@ -15,10 +30,19 @@ const SignIn = () => {
                 <Col className="d-flex flex-column justify-content-between p-4">
                     <h3>Sign In</h3>
                     <label>Email address</label>
-                    <Input placeholder="Email address" />
+                    <Input 
+                    placeholder="Email address" 
+                    type="email"
+                    value={email}
+                    onChange={(event)=>setEmail(event.target.value)}
+                    />
                     <label>Password</label>
-                    <Input placeholder="Password" />
-                    <Button className="mt-2" color="primary" size="lg" block>Sign In</Button>
+                    <Input
+                     placeholder="Password"
+                     type="password"
+                     onChange={(event)=>setPassword(event.target.value)}
+                      />
+                    <Button className="mt-2" color="primary" size="lg" block onClick={signInUser}>Sign In</Button>
                     <a href="/signup">Don't have an account?</a>
                 </Col>
             </Row>
