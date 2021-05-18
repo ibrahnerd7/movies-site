775f9af9ecb904a480c3d10ca7126b782adee84b
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-05-19 02:03:15 +0300

    Test search  saga

---
+++ src/application/sagas/__tests__/searchSaga.js
@@ -0,0 +1,35 @@
+import { runSaga } from 'redux-saga';
+import * as api from '../../../infrastructure/services/api/search'
+import { requestSearchError, requestSearchSuccess } from '../../actions-creators/search';
+import { } from '../../actions-creators/search';
+import { handleSearchLoad } from '../searchSaga';
+
+test('should load search results and handle them in case of success', async() => {
+    const dispatchedActions=[];
+    const mockedResults=[{id:'123',name:"The good doctor"}];
+
+    api.getSearchResults=jest.fn(()=>Promise.resolve(mockedResults))
+
+    const fakeStore={
+        dispatch:action=>dispatchedActions.push(action)
+    }
+
+    await runSaga(fakeStore,handleSearchLoad,"doctor").done;
+    expect(api.getSearchResults.mock.calls.length).toBe(1);
+    expect(dispatchedActions).toContainEqual(requestSearchSuccess(mockedResults))
+});
+
+test('should load  search results and handle them in case of fail', async() => {
+    const dispatchedActions=[]
+
+    const error="Some error was thrown";
+    api.getSearchResults=jest.fn(()=>Promise.reject(error))
+
+    const fakeStore={
+        dispatch:action=>dispatchedActions.push(action)
+    }
+
+    await runSaga(fakeStore,handleSearchLoad,"movie").done;
+    expect(api.getSearchResults.mock.calls.length).toBe(1);
+    expect(dispatchedActions).toContainEqual(requestSearchError())
+});
