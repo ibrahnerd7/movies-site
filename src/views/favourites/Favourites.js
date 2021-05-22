b12ab9769fcc4a06494669551aa59797de3ccd07
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-05-22 17:02:18 +0300

    Show favourites in list

--- src/views/favourites/Favourites.js
+++ src/views/favourites/Favourites.js
@@ -1,48 +1,50 @@
-import React, { useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useContext } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
-import { Row, Card, CardImg, CardBody, CardTitle, CardSubtitle, Col, Container, CardGroup } from 'reactstrap';
+import { Button, Col, Container } from 'reactstrap';
+import { IoMdTrash } from 'react-icons/io'
 import { requestFavourites } from '../../application/actions-creators/favourites';
 import { UserContext } from '../../application/providers/UserProvider';
 
-const FavouritesItem=({showItem})=>{
-    return(
-        <Col md="2" xs="6">
-        <Card className="mt-2">
-            <CardImg top src={`https://www.themoviedb.org/t/p/w440_and_h660_face${showItem.poster_path}`} alt="Card image cap" />
-            <CardBody>
-                <CardTitle tag="h6" className="text-nowrap text-truncate" style={{width:"100%"}}>{showItem.name}</CardTitle>
-                <CardSubtitle tag="p" className="mb-2 text-muted">{new Date(showItem.release_date).toLocaleDateString()}</CardSubtitle>
-            </CardBody>
-        </Card>
-        </Col>
+
+const FavouritesItem = ({ favourite }) => {
+    return (
+        <div class="card my-4 flex-row  border-0 rounded" style={{backgroundColor:"#24282d"}}>
+            <img  className="rounded img-fluid " style={{maxWidth:"15%",maxHeight:"40%"}} src={`https://www.themoviedb.org/t/p/w440_and_h660_face${favourite.poster_path}`} alt={favourite.title} />
+            <div class="card-body">
+                <h5 class="card-title text-white">{favourite.title}</h5>
+                <p class="card-text text-white">{favourite.tagline}</p>
+                <p class="card-text"><small class="text-muted">{new Date(favourite.release_date).toLocaleDateString()}</small></p>
+            </div>
+          <IoMdTrash   className="my-auto" color="#dc3545"/> 
+        </div>
     )
 }
 
 const Favourites = () => {
     const dispatch = useDispatch()
-    const user=useContext(UserContext);
-    const {favourites,loading}=useSelector(state=>state.favourites);
+    const user = useContext(UserContext);
+    const { favourites, loading } = useSelector(state => state.favourites);
 
     useEffect(() => {
-        if(user){
+        if (user) {
             dispatch(requestFavourites(user.uid));
         }
     }, [dispatch, user])
 
     return (
-        <Container>
-            <h1 className="mt-4 mb-4">Favourites</h1>
-            <Row className="flex-row">
-            <CardGroup>
+        <Container fluid style={{backgroundColor:"#24282d",paddingTop:16, paddingBottom:16}}>
+            <Container>
+            <h1 className="mb-4 text-white">Favourites</h1>
+            <div class="d-flex flex-column card-group">
                 {
-                    favourites.map((show) => (
-                     <FavouritesItem key={show.id} showItem={show} />
+                    favourites.map((favourite) => (
+                        <FavouritesItem key={favourite.id} favourite={favourite} />
                     ))
                 }
-                </CardGroup>
-            </Row>
+            </div>
+            </Container>
         </Container>
     )
 }
