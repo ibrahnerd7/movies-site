c24fcf8ac3a2277eaf22501cc60008a9bca5508c
ibrahnerd7 <ibrahmaina7@gmail.com>
2021-05-22 18:47:40 +0300

    Disable redux devtools in production builds

--- src/application/store.js
+++ src/application/store.js
@@ -11,7 +11,7 @@ import rootSaga from './sagas';
         rootReducer,
         compose(
             applyMiddleware(sagaMiddleware),
-            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+           ( !process.env.NODE_ENV || process.env.NODE_ENV === 'development') && window.__REDUX_DEVTOOLS_EXTENSION__ &&
             window.__REDUX_DEVTOOLS_EXTENSION__(),
         ),
     );
